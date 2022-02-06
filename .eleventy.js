d69b54572cb3cc1a0e5ddf99708f556bbd354eb1
Angelo Fallaria <ba.fallaria@gmail.com>
2022-02-07 04:02:20 +0800

    chore: move assets/css/ to css/

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,6 @@
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy('assets')
+  eleventyConfig.addPassthroughCopy('css')
   return {
     passthroughFileCopy: true
   }
