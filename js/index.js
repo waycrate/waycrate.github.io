4633afa19f7e959597f20eaa792a2661db8c31d9
angelofallars <angelofallars@users.noreply.github.com>
2022-02-09 01:12:59 +0000

    deploy: b86d41a00edbe65f4d58a56652d686c791c659f5

--- js/index.js
+++ js/index.js
@@ -1,6 +1,16 @@
 const darkModeToggle = document.querySelector(".dark-mode-toggle");
 const currentTheme = localStorage.getItem("theme");
 
+// If no current theme set in browser storage, set it based on
+// preferred theme
+if (localStorage.getItem("theme") === null) {
+  const prefersDarkScheme = window.matchMedia('(prefers-color-scheme: dark)');
+
+  if (prefersDarkScheme.matches) {
+    document.body.classList.add("dark");
+  }
+}
+
 if (currentTheme == "dark") {
   document.body.classList.add("dark");
 }
