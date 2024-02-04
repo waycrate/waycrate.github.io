c1d60485e1bc66d44f748fc0a12317dfacc92131
Shinyzenith <aakashsensharma@gmail.com>
2024-02-05 03:05:38 +0530

    feat: Add nix flake
    Signed-off-by: Shinyzenith <aakashsensharma@gmail.com>

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "Waycrate-website devel";
+
+  inputs = { nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable"; };
+
+  outputs = { self, nixpkgs, ... }:
+    let
+      pkgsFor = system:
+        import nixpkgs {
+          inherit system;
+          overlays = [ ];
+        };
+
+      targetSystems = [ "aarch64-linux" "x86_64-linux" ];
+    in {
+      devShells = nixpkgs.lib.genAttrs targetSystems (system:
+        let pkgs = pkgsFor system;
+        in {
+          default = pkgs.mkShell {
+            name = "Waycrate-website-devel";
+            nativeBuildInputs = with pkgs; [
+				nodejs
+            ];
+          };
+        });
+    };
+}
